096e151a5982941c754b4e3c0e6afbb05858623a
xsuuu <434893886@qq.com>
2026-05-08 14:55:39 +0800

    landing page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,23 +4,28 @@ import { Toaster } from 'react-hot-toast';
 
 import HomePage from './pages/HomePage';
 import ProblemsPage from './pages/ProblemsPage';
+import DashboardPage from './pages/DashboardPage';
 
 function App() {
-  const { isSignedIn, isLoaded } = useUser;
+  const { isSignedIn, isLoaded } = useUser();
+
+  //this will get rid of the flickering effect
+  if (!isLoaded) return null;
 
   return (
     <>
       <Routes>
-        <Route path='/' element={<HomePage />} />
+        <Route
+          path='/'
+          element={!isSignedIn ? <HomePage /> : <Navigate to={'/dashboard'} />}
+        />
+        <Route
+          path='/dashboard'
+          element={isSignedIn ? <DashboardPage /> : <Navigate to={'/'} />}
+        />
         <Route
           path='/problems'
-          element={
-            !isLoaded ? null : isSignedIn ? (
-              <ProblemsPage />
-            ) : (
-              <Navigate to={'/'} />
-            )
-          }
+          element={isSignedIn ? <ProblemsPage /> : <Navigate to={'/'} />}
         />
       </Routes>
       <Toaster position='top-center' toastOptions={{ duration: 3000 }} />
@@ -30,8 +35,8 @@ function App() {
 
 export default App;
 
-//tw, daisyui, react-router, react-hot-toast,
-//todo:react-query aka tanstack query, axios
+//tw, daisyui, react-router, react-hot-toast, react-query aka tanstack query, axios
+//todo:111
 //! ddddddddddd
 //? dddddddddd
 //* dddddddddd
